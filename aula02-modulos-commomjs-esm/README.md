1e60b7761faa6ccda91fb06d2010b78063acc4d9
Vitória Luane Malafaia Ferreira <luane2966@gmail.com>
2026-09-14 20:50:59 -0300

    feat (implementa sistema com logs como dados e tempo)

---
+++ aula02-modulos-commomjs-esm/README.md
@@ -0,0 +1,5 @@
+aula 02 
+Claro! Pelo que você fez hoje, um comentário legal para registrar seria:
+
+ Hoje trabalhei na implementação de um sistema de logs utilizando JavaScript com módulos ES6. Criei uma função formatLog() para adicionar data e hora aos registros e organizei a estrutura do projeto utilizando package.json com type: "module". Também implementei a função `salvarLogSistema()` para criar o diretório de logs, salvar as mensagens em um arquivo `syslog.log` e tratar possíveis erros durante o processo.
+
---
+++ aula02-modulos-commomjs-esm/index.js
@@ -0,0 +1,25 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { formatLog } from './utilitario.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname( __filename );
+
+async function salvarLogSistema(mensagemLog){
+ try{
+const pastaLog = path.join ( __dirname,'log');
+const arquivoLog =  path.join (pastaLog,'syslog.log');
+
+await fs.mkdir (pastaLog,{recursive:true});
+const registro =formatLog (mensagemLog);
+await fs.appendFile (arquivoLog,registro, 'utf-8');
+
+console.log ('Log Registrado com sucesso...');
+ }catch(erro){
+    console.error ('Erro ao registrar log: ',erro);
+ }   
+}
+salvarLogSistema ('Inicialização do servidor concluída!\n');
+salvarLogSistema ('conexão com banco de dados estabelecida!\n');
+
